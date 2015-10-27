07203972c9523bccf8709cf9759d3861af5ec2b3
Jeca De Alva <jecadealva@hotmail.com>
2015-10-27 17:02:16 -0600

    sistema solar

---
+++ MiBiblioteca/Assets/Scripts/Planeta.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planeta : MonoBehaviour {
+	public GameObject center;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//Permitee girar un objeto alrededor de otro
+	//Lo busque en google como: rotate around unity
+		transform.RotateAround (center.transform.position , Vector3.up, speed * Time.deltaTime);
+	}
+}
